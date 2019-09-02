a14ea7c640ea3d9c9316139433be73993f8fcdfa
Sasha Bemotoff <bemot@users.noreply.github.com>
2019-09-02 21:07:16 +0300

    Delete factory.js

--- ethereum/factory.js
+++
@@ -1,9 +0,0 @@
-import web3 from './web3';
-import CampaignFactory from './build/CampaignFactory.json';
-
-const instance = new web3.eth.Contract(
-  JSON.parse(CampaignFactory.interface),
-  '0xa3827b528b1d96a34369657e512cc7bf8d39c7f8'
-);
-
-export default instance;
